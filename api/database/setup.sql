05b7a7033e0848a3762afe7d2c079a700f6cf6c2
Franki Callard <francescalily@outlook.com>
2023-08-15 16:04:28 +0100

    inserting data bugs template

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -31,9 +31,17 @@ CREATE TABLE bug (
 
 INSERT INTO bug (name, continent, image_url, genus, ecology, description)
 VALUES
-('Grasshopper', 'All but Antartica', 'https://upload.wikimedia.org/wikipedia/commons/3/38/Grasshopper_at_MGSP.jpg', 'suborder Caelifera', 'herbivore', '
-A grasshopper is a jumping insect that feeds on plants.'),
-('Bee', 'All but Antartica', 'https://upload.wikimedia.org/wikipedia/commons/4/4d/Apis_mellifera_Western_honey_bee.jp30', 'Apis', '-', 'A bee is a flying insect known for pollination and producing honey.');
+('Grasshopper', 'All but Antarctica', 'https://upload.wikimedia.org/wikipedia/commons/3/38/Grasshopper_at_MGSP.jpg', 'suborder Caelifera', 'herbivore', 'A grasshopper is a jumping insect that feeds on plants.'),
+('Bee', 'All but Antarctica', 'https://upload.wikimedia.org/wikipedia/commons/4/4d/Apis_mellifera_Western_honey_bee.jpg', 'Apis', 'pollinator', 'A bee is a flying insect known for pollination and producing honey.'),
+('Butterfly', 'Worldwide', 'https://i.natgeofe.com/k/9acd2bad-fb0e-43a8-935d-ec0aefc60c2f/monarch-butterfly-grass_4x3.jpg', 'Rhopalocera', 'pollinator', 'A colorful flying insect, metamorphoses from caterpillar to butterfly.'),
+('Ant', 'Worldwide', 'https://images.unsplash.com/photo-1588470045344-4393b295297c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YW50fGVufDB8fDB8fHww&w=1000&q=80', 'Formicidae', 'omnivore', 'Social insects, known for their complex colonies and organization.'),
+('Dragonfly', 'Worldwide', 'https://upload.wikimedia.org/wikipedia/commons/0/03/Sympetrum_flaveolum_-_side_%28aka%29.jpg', 'Anisoptera', 'carnivore', 'Flying insects, skilled hunters with strong transparent wings.'),
+('Ladybug', 'Worldwide', 'https://www.arkwildlife.co.uk/wp-content/uploads/2014/05/ladybird.jpg', 'Coccinellidae', 'carnivore', 'Small beetles, often red with black spots, feed on pests.'),
+('Mantis', 'Worldwide', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/European_praying_mantis_%28Mantis_religiosa%29_green_female_Dobruja.jpg/1200px-European_praying_mantis_%28Mantis_religiosa%29_green_female_Dobruja.jpg', 'Mantodea', 'carnivore', 'Green or brown insects known for their "praying" forelimbs.'),
+('Mosquito', 'Worldwide', 'https://upload.wikimedia.org/wikipedia/commons/d/dc/Mosquito_2007-2.jpg', 'Culicidae', 'bloodsucker', 'Flying insects that feed on blood and can transmit diseases.'),
+('Beetle', 'Worldwide', 'https://images.immediate.co.uk/production/volatile/sites/22/2023/05/Stag-beetle-1a8d6ad.jpg', 'Coleoptera', 'omnivore', 'Diverse group of insects with hard outer wing cases.'),
+('Moth', 'Worldwide', 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Attacus_atlas_%2815050883139%29.jpg', 'Heterocera', 'herbivore', 'Night flying insects, related to butterflies, often attracted to light.');
+
  
 
 --INSERT INTO user_account (username, password, is_admin) VALUES ('admin', 'Password1', true);
