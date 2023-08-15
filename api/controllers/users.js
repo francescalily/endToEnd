1f1f8869456226cc7708ffdf4e3dd3e3f345bfa2
Franki Callard <francescalily@outlook.com>
2023-08-15 11:17:08 +0100

    adding user

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,23 +1,38 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const Token = require("../models/Token");
+const bcrypt = require("bcrypt");
 
 async function register(req, res) {
-    try {
-        res.send(200).send('Registration Completed!');
-    } catch (err) {
-        res.status(500).json({ err: err.message });
-    }
+  const data = req.body;
+  try {
+    const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS));
+    data["password"] = await bcrypt.hash(data["password"], salt);
+    const result = await User.create(data);
+    res.send(200).send("result");
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
 }
 
 async function login() {
-    try {
-        res.send(200).send('Login Completed!');
-    } catch (err) {
-        res.status(500).json({ err: err.message });
+  const data = req.body;
+  try {
+    const user = await User.getOneByUsername(data.username);
+    console.log("User", user);
+    const authenticated = await bcrypt.compare(data.password, user["password"]);
+    console.log("Authenticated", authenticated);
+    if (!authenticated) {
+      throw new Error("Incorrect credentials.");
+    } else {
+      const token = await Token.create(user.id);
+      res.status(200).json({ authenticated: true, token: token.token });
     }
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
 }
-
 
 module.exports = {
-    register,
-    login
-}
+  register,
+  login,
+};
